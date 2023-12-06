20ad73dd90116c7575411a7709f46769c466805b
xasanjon <xasanjonabdunabiyev222@gmail.com>
2023-12-06 19:06:29 +0500

    added

--- components/about.tsx
+++ components/about.tsx
@@ -19,21 +19,8 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
+        In 2021, I started the frontend course at <span className="font-medium">Albison Academy</span> on a grand basis and successfully completed the course and participated in several community projects. I have mentioned what technologies I know I am currently looking for a full time job as a software developer.
+        When I'm not coding, I enjoy playing video games, watching movies, and learning new things. I am also currently learning backend programming. My plan now is to become a full-stack in the short term.
       </p>
 
       <p>
